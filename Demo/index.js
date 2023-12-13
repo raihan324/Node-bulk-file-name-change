e8633185d654bd50573adbca53c97bcf94ad4066
Md Raihan <raihanu600@gmail.com>
2023-12-13 19:41:10 +0600

    Work Done

--- Demo/index.js
+++ Demo/index.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 const path = require('path')
 
-const filepath = '/users/raiha/OneDrive/Pictures/Screenshots'
-const file = fs.readdirSync(filepath).filter(kk => kk.endsWith('.png'))
-fs.readFile(file[0], (err, data) => {
-    console.log(err)
+const filepath = '/Users/raiha/oneDrive/Desktop/All_work_file/Javascript/Node bulk file name change/Screenshots'
+const files = fs.readdirSync(filepath).filter(kk => kk.endsWith('.png'))
+console.log(files)
+let count = 1
+const extName = '.png'
+
+files.forEach(file => {
+    const oldPath = path.join(filepath, file)
+    const newPath = path.join(filepath, count + extName)
+    fs.renameSync(oldPath, newPath)
+    console.log('Done ', count, ' ...')
+    count++
 })
-// console.log(file)
